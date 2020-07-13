9d1859a9c65c0a35885b2fdb2621140d50b5e5f9
nick <nicholasiozzo17@gmail.com>
2020-07-13 19:29:56 -0400

    - Added a list view activity and a button to go to that activity
    - Created Article class
    
    - Manually added 7 articles to the list view
    
    - Created NewsListAdapter class
    
    - When list view item is clicked, an alert shows up with the relevant information

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Temporary button to go to news list to test it
+        Button button = findViewById(R.id.button);
+
+        //Sets the onclicklistener for the button to go to the news list activity
+        button.setOnClickListener(v -> {
+            Intent newsListActivity = new Intent(MainActivity.this, NewsListActivity.class);
+            startActivity(newsListActivity);
+        });
     }
 }
