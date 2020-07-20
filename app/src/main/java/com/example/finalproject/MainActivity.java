28f839e1087514309c80219fec19e21d5c6d70c1
nick <nicholasiozzo17@gmail.com>
2020-07-20 15:20:46 -0400

    - Moved async task to newsActivity
    - Bug fixes for async task
    
    - Added Progress Bar
    
    - Added comments

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
-
+//Temporary activity please do not consider this an activity for the requirements
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,8 +14,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Button used to go to news list activity
         Button button = findViewById(R.id.button);
 
+        //On click listener for button gos to the news list activity when triggered
         button.setOnClickListener(v -> {
             Intent newsListActivity = new Intent(MainActivity.this, NewsListActivity.class);
             startActivity(newsListActivity);
