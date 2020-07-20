c49f9cdb7348c80b033607d7be274ebdc291fdcc
androidDeveloper15 <65261090+androidDeveloper15@users.noreply.github.com>
2020-07-20 02:16:43 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,24 +2,160 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    ListView articlesList;
+
+    ArrayList<String> titles;
+    ArrayList<String> descriptions;
+    ArrayList<String> links;
+    ArrayList<String> dates;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Temporary button to go to news list to test it
-        Button button = findViewById(R.id.button);
+        articlesList = (ListView) findViewById(R.id.list_view);
 
-        //Sets the onclicklistener for the button to go to the news list activity
-        button.setOnClickListener(v -> {
-            Intent newsListActivity = new Intent(MainActivity.this, NewsListActivity.class);
-            startActivity(newsListActivity);
+        titles = new ArrayList<String>();
+        descriptions = new ArrayList<String>();
+        links = new ArrayList<String>();
+        dates = new ArrayList<String>();
+
+        articlesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Uri uri = Uri.parse(links.get(position));
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
         });
+        new ArticleQuery().execute();
+    }
+
+    public InputStream getInputStream(URL url) {
+
+        try {
+            return url.openConnection().getInputStream();
+
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    public class ArticleQuery extends AsyncTask<Integer, Void, Exception> {
+
+        Exception exception = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+
+        @Override
+        protected Exception doInBackground(Integer... integers) {
+
+            try {
+                URL url = new URL("http://feeds.bbci.co.uk/news/world/us_and_canada/rss.xml");
+
+                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+
+                factory.setNamespaceAware(false);
+
+                XmlPullParser xpp = factory.newPullParser();
+
+                xpp.setInput(getInputStream(url), "UTF-8");
+
+                boolean insideItem = false;
+
+                int eventType = xpp.getEventType();
+
+                // looping document to extract title, description, link, date
+                while (eventType != XmlPullParser.END_DOCUMENT) {
+
+                    if (eventType == XmlPullParser.START_TAG) {
+
+                        if (xpp.getName().equalsIgnoreCase("item")) {
+                            insideItem = true;
+                        } else if (xpp.getName().equalsIgnoreCase("title")) {
+
+                            if (insideItem) {
+                                titles.add(xpp.nextText());
+                            }
+                        } else if (xpp.getName().equalsIgnoreCase("description")) {
+
+                            if (insideItem) {
+                                descriptions.add(xpp.nextText());
+
+                            }
+                        } else if (xpp.getName().equalsIgnoreCase("link")) {
+
+                            if (insideItem) {
+                                links.add(xpp.nextText());
+                            }
+                        } else if (xpp.getName().equalsIgnoreCase("pubDate")) {
+
+                            if (insideItem) {
+                                dates.add(xpp.nextText());
+                            }
+                        }
+                    }
+
+                    else if (eventType == XmlPullParser.END_TAG && xpp.getName().equalsIgnoreCase("item")) {
+                        insideItem = false;
+                    }
+
+                    eventType = xpp.next();
+
+                }
+
+            } catch (MalformedURLException e) {
+                exception = e;
+            } catch (XmlPullParserException e) {
+                exception = e;
+            } catch (IOException e) {
+                exception = e;
+            }
+
+            return exception;
+        }
+
+        @Override
+        protected void onPostExecute(Exception s) {
+            super.onPostExecute(s);
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,titles);
+            articlesList.setAdapter(adapter);
+
+
+        }
     }
 }
+
