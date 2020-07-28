d003a8724089295f2ab2a55fcf39defe32083875
nick <nicholasiozzo17@gmail.com>
2020-07-27 20:01:10 -0400

    - Added async task to settings activity
    - uses an image api to get a random background image for the settings activity

--- app/src/main/java/com/example/finalproject/ProfileActiviy.java
+++ app/src/main/java/com/example/finalproject/ProfileActiviy.java
@@ -1,21 +1,32 @@
 package com.example.finalproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.util.Log;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import java.nio.charset.StandardCharsets;
 public class ProfileActiviy extends BaseActivity {
 
     private static final int REQUEST_IMAGE_CAPTURE = 1;
@@ -23,6 +34,10 @@ public class ProfileActiviy extends BaseActivity {
     private EditText firstNameEditText;
     private EditText lastNameEditText;
     private EditText emailEditText;
+
+    RelativeLayout backgroundImageContainer;
+
+    Bitmap image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +48,10 @@ public class ProfileActiviy extends BaseActivity {
         setTitle("Profile");
 
         final ImageButton pictureButton = findViewById(R.id.picture_button);
+
+        backgroundImageContainer = findViewById(R.id.background_image_container);
+
+        new BackgroundImageQuery().execute();
 
         firstNameEditText = findViewById(R.id.first_name_edit_text);
         lastNameEditText = findViewById(R.id.last_name_edit_text);
@@ -53,6 +72,10 @@ public class ProfileActiviy extends BaseActivity {
             String email = emailEditText.getText().toString();
             //TO-DO save the data as shared preference
         });
+    }
+
+    public void updateBackgroundImage () {
+        backgroundImageContainer.setBackground(new BitmapDrawable(getApplicationContext().getResources(), image));
     }
 
     @Override
@@ -87,11 +110,76 @@ public class ProfileActiviy extends BaseActivity {
         Snackbar snackbar = Snackbar.make(profileView, snackbarMessage, duration);
 
         //This action will hide the article if confirmed
-        snackbar.setAction("Retake", v -> {
-            dispatchTakePictureIntent();
-        });
+        snackbar.setAction("Retake", v -> dispatchTakePictureIntent());
 
         //Shows the snackbar
         snackbar.show();
+    }
+
+    private class BackgroundImageQuery extends AsyncTask<String, Integer, String> {
+
+        private final String ACCESS_KEY = "8Rb5ana9LDe_4_n78eZ_gciKw-HURz34SSdLKjoD-kM";
+
+        @Override
+        protected String doInBackground(String... args) {
+
+            try {
+                //url where we get the articles from
+                publishProgress(10);
+                URL url = new URL("https://api.unsplash.com/photos/random?client_id=" + ACCESS_KEY);
+
+                //url connection
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                InputStream response = urlConnection.getInputStream();
+                //setProgress(10);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(response, StandardCharsets.UTF_8));
+                publishProgress(20);
+                StringBuilder sb = new StringBuilder();
+                publishProgress(30);
+                String line;
+                publishProgress(40);
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line).append("\n");
+                }
+                JSONObject jObject = new JSONObject(sb.toString());
+                publishProgress(50);
+                JSONObject urls = jObject.getJSONObject("urls");
+                publishProgress(60);
+                URL imageURL = new URL(urls.getString("raw"));
+                publishProgress(70);
+                HttpURLConnection imageURLConnection = (HttpURLConnection) imageURL.openConnection();
+                publishProgress(80);
+                InputStream imageInputStream = imageURLConnection.getInputStream();
+                publishProgress(90);
+                image = BitmapFactory.decodeStream(imageInputStream);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            publishProgress(100);
+            return "Done";
+        }
+
+        @Override
+        protected void onProgressUpdate (Integer... values) {
+            //Get the progress bar
+            final ProgressBar progressBar = findViewById(R.id.progressBar);
+
+            //Make the progress bar visible
+            progressBar.setVisibility(View.VISIBLE);
+
+            //Set the progress
+            progressBar.setProgress(values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            //Make progress bar invisible
+            final ProgressBar progressBar = findViewById(R.id.progressBar);
+            progressBar.setVisibility(View.INVISIBLE);
+            updateBackgroundImage();
+        }
     }
 }
